857048e92857690c4373a319182cb4c09b91cd1d
Yash <yashharale2003@gmail.com>
2022-06-04 21:58:19 +0530

    Initiated key-button slider

--- script.js
+++ script.js
@@ -205,6 +205,8 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img))
 
 // Slider
+const slider = function() {
+
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
@@ -213,21 +215,22 @@ const dotContainer = document.querySelector('.dots');
 let curSlide = 0;
 const maxSlide = slides.length;
 
-// const slider = document.querySelector('.slider');
-// slider.style.transform = 'scale(0.3) translateX(-800px)';
-// slider.style.overflow = 'visible';
-
+// Functions
 const createDots = function() {
   slides.forEach(function(_, i){
     dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
   });
 };
-createDots();
+
+const activateDot = function(slide) {
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+};
 
 const goToSlide = function(slide) {
   slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
 };
-goToSlide(0);
 
 // Next and Previous Slides
 const nextSlide = () => {
@@ -237,6 +240,7 @@ const nextSlide = () => {
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 const prevSlide = () => {
@@ -246,13 +250,22 @@ const prevSlide = () => {
     curSlide--;
   }
   goToSlide(curSlide)
+  activateDot(curSlide);
 };
 
+const init = function() {
+ goToSlide(0);
+ createDots();
+ activateDot(0);
+};
+init();
+
+// Event handlers
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
 
 document.addEventListener('keydown', function(e) {
-  console.log(e);
+  // console.log(e);
   e.key === 'ArrowRight' && nextSlide(); // short circuiting
   e.key === 'ArrowLeft' && prevSlide();
 });
@@ -261,10 +274,12 @@ dotContainer.addEventListener('click', function(e) {
   if(e.target.classList.contains('dots__dot')){
     const {slide} = e.target.dataset;
     goToSlide(slide);
+    activateDot(slide);
   }
 });
 
-
+};
+slider();
 
 
 //// Lectures
