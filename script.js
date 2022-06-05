c43906bd3f440b387266a584bf7abd974543309d
Yash <yashharale2003@gmail.com>
2022-06-05 22:09:37 +0530

    DOM lifecycling

--- script.js
+++ script.js
@@ -462,9 +462,17 @@ slider();
   //   if (el !== h1) el.style.color = 'purple';
   // })
 
+  document.addEventListener('DOMContentLoaded', function(e) {
+    console.log('HTML parsed and DOM tree built!', e);
+  });
 
-  // Sticky navigation: Intersection Observer API
+  window.addEventListener('load', function(e) {
+    console.log('Page fully loaded', e);
+  });
 
-
-
+  // window.addEventListener('beforeunload', function(e) {
+  //   e.preventDefault();
+  //   console.log(e);
+  //   e.returnValue = '';
+  // })
 
