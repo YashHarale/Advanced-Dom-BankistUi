7044d892d15c1238080b7fa3efba31bd9c6bedcb
Yash <yashharale2003@gmail.com>
2022-06-09 18:45:02 +0530

    Reviewed code

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
-  }
+  };
 });
 
 // Scrolling
