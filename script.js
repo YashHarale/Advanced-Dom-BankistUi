89c9d6101535b658f52657a9c1b5e8edb8477f7c
Yash <yashharale2003@gmail.com>
2022-06-11 01:05:44 +0530

    Reviewed code

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
   // Matching strategy
   if(e.target.classList.contains('nav__link')){
     const id = e.target.getAttribute('href');
-    console.log(id);
+    // console.log(id);
     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
   }
 });
